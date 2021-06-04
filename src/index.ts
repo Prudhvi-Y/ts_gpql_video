b7440c645b64247c8541323cd1c151bacb0bd625
Prudhvi-Y <prudhvi200@gmail.com>
2021-06-03 18:18:45 -0700

    initial setup
    Signed-off-by: Prudhvi-Y <prudhvi200@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import "reflect-metadata";
+import { __prod__ } from "./constants";
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { PrismaClient } from '@prisma/client';
+import http from 'http';
+import * as dotenv from 'dotenv';
+import { MyContext } from './types';
+import { HelloResolver } from './resolvers/hello';
+
+
+dotenv.config({path: __dirname+'../.env'});
+
+const main = async () => {
+    const prisma = new PrismaClient();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer ({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+        context: ({req, res}): MyContext => ({ prisma, req, res}),
+        subscriptions: {
+            path: "/subscriptions",
+            onConnect: (_connectionParams, _websocket) => {
+                console.log('connected to websocket');
+                // if (connectionParams.hasOwnProperty('Authorization')){
+                //     // console.log(connectionParams as string);
+                //     const parsed: subsAuth = connectionParams as subsAuth;
+                //     // console.log(parsed.Authorization);
+                //     const token = parsed.Authorization.replace('Bearer ','');
+                //     const userId = getTokenPayload(token);
+                //     if (!userId) {
+                //         return false;
+                //     } else {
+                //         return true;
+                //     }
+                // } else {
+                //     return false;
+                // }
+                
+            },
+            onDisconnect: () => console.log('disconnected from websocket'),
+        },
+    });
+
+    apolloServer.applyMiddleware({app});
+
+    const httpServer = http.createServer(app);
+    apolloServer.installSubscriptionHandlers(httpServer);
+
+    httpServer.listen(process.env.PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`);
+        console.log(`🚀 Subscriptions ready at ws://localhost:${process.env.PORT}${apolloServer.subscriptionsPath}`);
+    })
+}
+
+main().catch((err) => {
+    console.error(err);
+});
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+export type MyContext = {
+    prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>;
+    req: Request;
+    res: Response;
+}
+
+export interface subsAuth {
+    Authorization: string,
+};
