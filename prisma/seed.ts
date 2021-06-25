875c4c3de62a889ab38151ee0adb1953fe6d2ce5
Prudhvi-Y <prudhvi200@gmail.com>
2021-06-25 15:01:41 -0700

    seeding is working for admin and roles table
    Signed-off-by: Prudhvi-Y <prudhvi200@gmail.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export async function seed() {
+export async function main() {
     const superadmin = await prisma.roles.upsert({
         where: {
-            rolename: process.env.SUPERADMIN_ROLE,
+            rolename: process.env.SUPERADMIN_ROLE as string,
         },
         update: {},
         create: {
@@ -25,21 +25,32 @@ export async function seed() {
             usersedit: true,
             usersview: true,
             isadmin: true,
-            rolename: process.env.SUPERADMIN_ROLE,
+            rolename: process.env.SUPERADMIN_ROLE as string,
         },
-    })
+    });
 
     const admin = await prisma.admin.upsert({
         where: {
-            email: process.env.SUPERADMIN_EMAIL,
+            email: process.env.SUPERADMIN_EMAIL as string,
         },
         update: {},
         create: {
             admin: true,
-            email: process.env.SUPERADMIN_EMAIL,
-            name: process.env.SUPERADMIN_NAME,
-            password: process.env.SUPERADMIN_PASSWORD,
-            role: process.env.SUPERADMIN_ROLE,
+            email: process.env.SUPERADMIN_EMAIL as string,
+            name: process.env.SUPERADMIN_NAME as string,
+            password: process.env.SUPERADMIN_PASSWORD as string,
+            role: process.env.SUPERADMIN_ROLE as string,
         }
-    })
+    });
+
+    console.log(superadmin, admin);
 }
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
